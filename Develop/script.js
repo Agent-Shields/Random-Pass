c6571a36fceb619ae3cbd5a4afa2b558cb8708e0
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2021-12-11 23:57:40 -0600

    crit 2, 3 and 4 done, need to add code to ensure at least 1 char type confirmed before crit 5 completed

--- Develop/script.js
+++ Develop/script.js
@@ -3,21 +3,25 @@
   //Declare lower case characters
 
   let lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  // console.log(lowerCaseChar);
 
   //Declare upper case characters
 
   let upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    // console.log(upperCaseChar);
-
+ 
   //Declare special characters
 
-  // let specialChar = []
+  let specialChars = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', '(', ')', '{', '}', '[', ']', '~', '-', '_', '.']
+ 
+  //Declare numeric characters
 
-// Prompt user for password, 
+  let numericChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+// Prompt user for password
+
 function generatePassword() {
 
   //Request desired password criteria info
+
     function pwCriteria() {
 
         //get desired password Length
@@ -27,43 +31,86 @@ function generatePassword() {
 
         //check validity of user input
 
-      if (pwLength == "" || pwLength === null || pwLength === undefined) {
-        window.alert("you must enter a numeric value!");
-        return pwCriteria();
-      }
+        if (pwLength == "" || pwLength === null || pwLength === undefined) {
+          window.alert("you must enter a numeric value!");
+          return pwCriteria();
+        }
 
-        //convert string input value to integer 
+        //convert string input value to integer for pass/fail
 
         pwLength = parseInt(pwLength);
         console.log(pwLength);
 
-      if (!pwLength) {
-        window.alert("you must enter a numeric value!");
-        return pwCriteria();
-      } 
+        if (!pwLength) {
+          window.alert("you must enter a numeric value!");
+          return pwCriteria();
+        } 
 
-      if (pwLength < 8 || pwLength > 180) {
-        window.alert('You must enter a password length between 8 minimum and 180 maximum characters for your password!');
-        return pwCriteria();
-      }
+        if (pwLength < 8 || pwLength > 180) {
+          window.alert('You must enter a password length between 8 minimum and 180 maximum characters for your password!');
+          return pwCriteria();
+        }
 
         //verify desired password Length
 
-      window.confirm('You want your password to be ' + pwLength + ' characters long?');
-
-      // if (pwLength < 8 || pwLength > 180) {
-      //   window.alert('You must enter a password length between 8 minimum and 180 maximum characters for your password!')
-      //   return pwCriteria()
-      // }
+        var pwLengthConfirm = window.confirm('You want your password to be ' + pwLength + ' characters long?');
+        if (pwLengthConfirm) {
+          window.alert('Your password will be ' + pwLength + ' characters long')
+        }
+        else
+        {
+          return pwCriteria();
+        }
     
-        //get desired Password Character Types 
+        //get desired password character Types 
+        
+        //Ask user if want lowercase characters
+        var pwCharTypeLower = window.confirm('Do you want your password to include lower case characters?');
 
-      var pwCharType = window.prompt('Please Enter your desired password character types from this list: "LOWERCASE", "UPPERCASE", "NUMERIC" and or "SPECIAL CHARACTERS" ');
-      window.confirm('You want your password to contain ' + pwCharType + ' ? ');
-    }
+        //Verify user wants lowercase characters 
+
+        if (pwCharTypeLower) {
+          window.alert('Your password will contain lower case characters')
+        } else {
+          window.alert('Your password will not contain lower case characters')
+        }
+
+         //Ask user if want uppercase characters
+         var pwCharTypeUpper = window.confirm('Do you want your password to include upper case characters?');
+
+         //Verify user wants uppercase characters 
+
+         if (pwCharTypeUpper) {
+           window.alert('Your password will contain upper case characters')
+         } else {
+           window.alert('Your password will not contain upper case characters')
+         }
+
+          //Ask user if want numeric characters
+        var pwCharTypeNumeric = window.confirm('Do you want your password to include numeric characters?');
+
+        //Verify user wants numeric characters 
+
+        if (pwCharTypeNumeric) {
+          window.alert('Your password will contain numbers')
+        } else {
+          window.alert('Your password will not contain numbers')
+        }
+
+        //Ask user if want special characters
+        var pwCharTypeSpecial = window.confirm('Do you want your password to include special characters?');
+
+        //Verify user wants special characters 
+
+        if (pwCharTypeSpecial) {
+          window.alert('Your password will contain special characters')
+        } else {
+          window.alert('Your password will not contain special characters')
+        }
+      }
 
     pwCriteria();
-  
+
 }
 
 // Get references to the #generate element
