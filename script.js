6df2ada064c867c736a9081c41488f6751008836
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2021-12-12 18:44:15 -0600

    figuring out random numbers

--- script.js
+++ script.js
@@ -6,32 +6,32 @@
  
   //declare random lower case char from array
   
-    let lowerCaseChar = 'abcdefghijklmnopqrstuvwxyz'
+    let lowerCaseChar = 'abcdefghijklmnopqrstuvwxyz';
 
-     var pickedLowChar = lowerCaseChar[Math.floor(Math.random()*lowerCaseChar.length)]
+     var pickedLowChar = lowerCaseChar[Math.floor(Math.random()*lowerCaseChar.length)];
   
   //Declare upper case characters
 
   // var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-    let upperCaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let upperCaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-      var pickedUpChar = upperCaseChar[Math.floor(Math.random()*upperCaseChar.length)]
+      var pickedUpChar = upperCaseChar[Math.floor(Math.random()*upperCaseChar.length)];
 
   //Declare special characters
 
   // var specialChars = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', '(', ')', '{', '}', '[', ']', '~', '-', '_', '.']
 
 
-    let specialChars = "@%+/'!#$^?:,(){}[]~-_."
+    let specialChars = "@%+/'!#$^?:,(){}[]~-_.";
 
-      var pickedSpecChar = specialChars[Math.floor(Math.random()*specialChars.length)]
+      var pickedSpecChar = specialChars[Math.floor(Math.random()*specialChars.length)];
 
   //Declare numeric characters
 
   // var numericChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-    let numericChars = "0123456789"
+    let numericChars = "0123456789";
 
       var pickedNumVal = numericChars[Math.floor(Math.random()*numericChars.length)];
 
